8d079e80bd00dea29423f97690d735f55b3cac19
stephenleejm <stephenleejm@gmail.com>
2021-05-22 18:00:31 +0800

    Changed more to parameters like repo and last_date_time

--- arti_api.py
+++ arti_api.py
@@ -4,29 +4,37 @@ import json
 from pathlib import Path
 import os
 import datetime
+'''
+This program will find all the artifacts since the last update and download it to output_dir
+'''
 
+if __name__ == "__main__":
+    output_dir = "./export"
+    last_date_time_iso = "2021-05-22T13:45:00+08:00"
+    repo = "maven-challenge"
+    root_url = "http://192.168.1.96:8082/artifactory"
 
-output_dir = "./export"
+    # Call the AQL query to find new artefacts created since last_date_time
+    auth = HTTPBasicAuth('admin', 'P@ssw0rd')
+    query = 'items.find({"type":"file","created" : {"$gt" : "%s"},"repo" : "%s"})' % \
+            (last_date_time_iso, repo)
 
-auth = HTTPBasicAuth('admin', 'P@ssw0rd')
-query = 'items.find({"type":"file","created" : {"$gt" : "2021-05-22T13:45:00+08:00"},"repo" : "maven-challenge"})'
-root_url = "http://192.168.1.96:8082/artifactory"
-r = requests.post(root_url+"/api/search/aql", auth=auth, data=query)
-print(r.text)
+    r = requests.post(root_url+"/api/search/aql", auth=auth, data=query)
+    print(r.text)
 
-j = json.loads(r.text)
+    j = json.loads(r.text)
 
-latest_time = datetime.datetime.fromisoformat("2021-05-22T13:45:00+08:00")
+    latest_time = datetime.datetime.fromisoformat("2021-05-22T13:45:00+08:00")
 
-for r in j["results"]:
+    for r in j["results"]:
 
-    print(r["path"]+"\t"+r["created"])
-    dir_path = os.path.join(output_dir, r["path"])
+        print(r["path"]+"\t"+r["created"])
+        dir_path = os.path.join(output_dir, r["path"])
 
-    full_url = root_url +"/"+r["repo"]+"/"+r["path"]+"/"+r["name"]
+        full_url = root_url +"/"+r["repo"]+"/"+r["path"]+"/"+r["name"]
 
-    response = requests.get(full_url)
-    Path(dir_path).mkdir(parents=True, exist_ok=True)
+        response = requests.get(full_url)
+        Path(dir_path).mkdir(parents=True, exist_ok=True)
 
-    file_path = os.path.join(dir_path, r["name"])
-    Path(file_path).write_bytes(response.content)
+        file_path = os.path.join(dir_path, r["name"])
+        Path(file_path).write_bytes(response.content)
